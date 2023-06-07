8ac8691a4705096f7446bc83e97d6c345351eab9
Josh Merrill <joshmmerrill@outlook.com>
2023-06-07 16:29:48 +0100

    ARCH: Updated app architecture and scaffolded out JSON data file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './index.css'
 import Navbar from './components/nav/Nav'
 import Hero from './components/hero/Hero'
-import Wine from './components/wine/Wine'
+import Wine from './components/menu/Menu'
 import About from './components/about/About'
 import Footer from './components/footer/Footer'
 import Cart from './components/cart/Cart'

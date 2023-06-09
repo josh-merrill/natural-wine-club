f20eb8017c01d829bb549faa860c1eb5f96530a6
Josh Merrill <joshmmerrill@outlook.com>
2023-06-09 11:03:24 +0100

    FIX: Updating menu file path #3

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,6 +1,6 @@
 import "./menu.css";
 import Wine from "./Wine";
-import menuData from "../../data/menu.json";
+import menuData from "../data/menu.json";
 
 export default function Menu() {
   const { menu } = menuData;

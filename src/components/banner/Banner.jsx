79a2ab3725333e212b2561d3f88f395b7b8260f9
Josh Merrill <joshmmerrill@outlook.com>
2023-06-09 11:18:13 +0100

    FEAT: Added app banner content and styling

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,9 +1,21 @@
 import "./banner.css";
+import wavyBorderTop from "../../assets/orange-wave-top.svg";
+import wavyBorderBottom from "../../assets/orange-wave-bottom.svg";
 
 export default function Banner() {
   return (
     <div className="banner--container">
-
+      <div className="curved-div upper">
+        <img src={wavyBorderTop} alt="A beautiful wavy border" />
+      </div>
+      <div className="curved-div">
+        <h3>
+          For natural wine lovers • For natural wine lovers • For natural wine lovers • For natural wine lovers • For natural wine lovers • For natural wine lovers
+        </h3>
+        </div>
+        <div className="curved-div-lower">
+        <img src={wavyBorderBottom} alt="A beautiful wavy border" />
+      </div>
     </div>
   );
 }

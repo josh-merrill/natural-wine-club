f79b87587c310a54d3e044788061258059c8f9d3
Josh Merrill <joshmmerrill@outlook.com>
2023-06-09 11:19:37 +0100

    FIX: Update to banner html classNames

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,15 +5,15 @@ import wavyBorderBottom from "../../assets/orange-wave-bottom.svg";
 export default function Banner() {
   return (
     <div className="banner--container">
-      <div className="curved-div upper">
+      <div className="banner--wrapper-upper">
         <img src={wavyBorderTop} alt="A beautiful wavy border" />
       </div>
-      <div className="curved-div">
+      <div className="banner--wrapper">
         <h3>
           For natural wine lovers • For natural wine lovers • For natural wine lovers • For natural wine lovers • For natural wine lovers • For natural wine lovers
         </h3>
         </div>
-        <div className="curved-div-lower">
+        <div className="banner--wrapper-lower">
         <img src={wavyBorderBottom} alt="A beautiful wavy border" />
       </div>
     </div>

1c62a7e7e2ff9b288dfa5d71b2ea969337983d1a
Josh Merrill <joshmmerrill@outlook.com>
2023-06-13 10:55:01 +0100

    STYLE: Added first commit for footer with basic styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,41 @@
-import './footer.css'
+import "./footer.css";
+import footerLogo from "../../assets/footer-logo.svg";
 
 export default function Footer() {
   return (
-    <footer className="container">
+    <footer>
       <div className="footer--container">
-        <h2>Footer</h2>
+        <div className="footer--column">
+          <img className="footer--logo" src={footerLogo} />
+        </div>
+        <div className="footer--column">
+          <ul>
+            <li>All wines</li>
+            <li>Stocklist</li>
+            <li>About Us</li>
+          </ul>
+        </div>
+        <div className="footer--column">
+          <ul>
+            <li>Get in Touch</li>
+            <li>Shipping</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer--column">
+          <h4>Distributed from</h4>
+          <p>Washington 4920 SW Ober Beach Rd. Vashon, WA 98070,
+            USA
+          </p>
+        </div>
+        <div className="footer--column">
+          <h4>NWC Radio</h4>
+          <ul>
+            <li>Link 10</li>
+            <li>Link 11</li>
+          </ul>
+        </div>
       </div>
     </footer>
-  )
+  );
 }

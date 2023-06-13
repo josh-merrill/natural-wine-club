1bf055e1bb274fa92ca7d3be959b71223ed4003b
Josh Merrill <joshmmerrill@outlook.com>
2023-06-13 20:55:33 +0100

    STYLE: Updating footer style

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,41 +1,48 @@
 import "./footer.css";
 import footerLogo from "../../assets/footer-logo.svg";
+import FacebookMsgSvg from "../../assets/facebook-messenger.svg";
+import InstagramSvg from "../../assets/instagram.svg";
+import TwitterSvg from "../../assets/twitter.svg";
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="footer--container">
-        <div className="footer--column">
-          <img className="footer--logo" src={footerLogo} />
-        </div>
-        <div className="footer--column">
-          <ul>
-            <li>All wines</li>
-            <li>Stocklist</li>
-            <li>About Us</li>
-          </ul>
-        </div>
-        <div className="footer--column">
-          <ul>
-            <li>Get in Touch</li>
-            <li>Shipping</li>
-            <li>Privacy Policy</li>
-          </ul>
-        </div>
-        <div className="footer--column">
-          <h4>Distributed from</h4>
-          <p>Washington 4920 SW Ober Beach Rd. Vashon, WA 98070,
-            USA
-          </p>
-        </div>
-        <div className="footer--column">
-          <h4>NWC Radio</h4>
-          <ul>
-            <li>Link 10</li>
-            <li>Link 11</li>
-          </ul>
-        </div>
+    <footer className="footer--container">
+      <a href="#" className="footer--logo">
+        <img src={footerLogo} alt="Logo"/>
+      </a>
+
+      <div className="footer--social">
+          <a href="#">
+            <img src={FacebookMsgSvg}  alt="Messenger" className="footer--social-icon" />
+          </a>
+          <a href="#">
+          <img src={InstagramSvg}  alt="Instagram" className="footer--social-icon" />
+          </a>
+          <a href="#">
+          <img src={TwitterSvg}  alt="Twitter" className="footer--social-icon" />
+          </a>
       </div>
-    </footer>
+
+      <div className="footer--links col1">
+        <a href="#">All Wines</a>
+        <a href="#">Blog</a>
+        <a href="#">About Us</a>
+      </div>
+
+      <div className="footer--links col2">
+        <a href="#">FAQs</a>
+        <a href="#">Support</a>
+        <a href="#">Privacy Policy</a>
+      </div>
+
+      <div className="footer--cta">
+        <button className="btn-light">Subscribe</button>
+      </div>
+      
+      <div className="footer--copyright">
+        <p>&copy; NWC. All Rights Reserved.</p>
+      </div>
+
+</footer>
   );
 }

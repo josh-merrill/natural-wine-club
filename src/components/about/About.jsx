98f943757051cc7e2d3c9f48cbfe058387f7cf53
Josh Merrill <joshmmerrill@outlook.com>
2023-06-27 15:36:48 +0100

    STYLE: Adjusted JSON data and styling for menu and cart

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,10 +8,8 @@ export default function About() {
         <div className="about--container-top">
           <h2>What is natural wine anyway?</h2>
           <p className="about--container-text">
-            Lorem ipsum dolor sit amet consectetur. Et est malesuada purus et
-            hendrerit tellus egestas in. Lobortis eu id malesuada nec turpis
-            praesent dignissim. Rhoncus aliquam vel scelerisque turpis ut augue
-            faucibus elementum.
+          Ah, natural wine! It's like the wild child of the wine world, the rebel without a cork. Picture this: a vineyard where the grapes frolic freely, basking in the sunshine and doing their organic yoga poses. No chemicals or pesticides dare to disturb their zen vibes.</p>
+          <p className="about--container-text">When the time is ripe, these precious grapes are plucked by gentle hands, as if they were chosen for a grape beauty pageant. Then, they're fermented with native yeasts, and bottled without any additives or preservatives. The result? A wine that's as pure as the day it was born.
           </p>
         </div>
           <img
@@ -29,10 +27,7 @@ export default function About() {
         <div className="about--container-bottom">
           <h2>So how exactly does this work?</h2>
           <p className="about--container-text">
-            Lorem ipsum dolor sit amet consectetur. Et est malesuada purus et
-            hendrerit tellus egestas in. Lobortis eu id malesuada nec turpis
-            praesent dignissim. Rhoncus aliquam vel scelerisque turpis ut augue
-            faucibus elementum.
+          With just a few clicks, you can add your favorite natural wines to your virtual cart, and voila! They'll embark on a daring journey, braving treacherous roads and evading sneaky delivery trolls, just to arrive at your doorstep. It's like a wine-themed adventure movie, but without the drama and with way more delicious sips.
           </p>
         </div>
       </div>

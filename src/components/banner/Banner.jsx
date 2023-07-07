d6190e3dd5aaf0c723372fcce66c2ca03362392d
Josh Merrill <joshmmerrill@outlook.com>
2023-07-07 12:08:12 -0700

    REFACTOR: Small update to Banner component

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -3,7 +3,6 @@ import "./banner.css"
 import Reveal from "../../utils/Reveal";
 
 export default function Banner() {
-
   const [scrollXPosition, setScrollXPosition] = useState(0)
 
   useEffect(() => {
@@ -25,12 +24,12 @@ export default function Banner() {
 
   return (
     <Reveal>
-    <section className="banner--wrapper">
-      <h3 style={styles}>
-        For natural wine lovers • For organic seekers and sustainable sippers • For those who know that less is more •
-        For natural wine lovers • For organic seekers and sustainable sippers • For those who know that less is more •
-      </h3>
-    </section>
+      <section className="banner--wrapper">
+        <h3 style={styles}>
+          For natural wine lovers • For organic seekers and sustainable sippers • For those who know that less is more •
+          For natural wine lovers • For organic seekers and sustainable sippers • For those who know that less is more •
+        </h3>
+      </section>
     </Reveal>
   )
 }
